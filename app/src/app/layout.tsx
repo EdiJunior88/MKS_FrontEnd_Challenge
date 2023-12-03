780d8946e5bb88d384f1b38a401e255f57a8f731
EdiJunior88 <er.moura88@yahoo.com.br>
2023-12-03 14:32:58 -0300

    - Update

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Favicon from "/public/favicon.ico";
 import "@/app/css/reset.css";
 import StyledComponentsRegistry from "@/app/registry";
